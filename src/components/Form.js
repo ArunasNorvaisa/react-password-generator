e18b398c4b41d284425b8e33669cca56389ee8f7
Arūnas Norvaiša <an@86.lt>
2018-10-27 15:05:42 +0300

    methods optimization

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,17 +6,17 @@ class Form extends Component {
             {
                 name: "Lowercase characters",
                 characters: "abcdefghijklmnopqrstuvwxyz",
-                selected: true
+                selected: false
             },
             {
                 name: "Uppercase characters",
                 characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-                selected: true
+                selected: false
             },
             {
                 name: "Symbols",
                 characters: "!@#$%^&*()_-+=|{}[]].,;:?\\/<>'\"",
-                selected: true
+                selected: false
             },
             {
                 name: "Digits",
@@ -24,47 +24,46 @@ class Form extends Component {
                 selected: true
             }
         ],
-        passwordLength: 16,
-        numberOfPasswords: 9
+        passwordLength: 10,
+        numberOfPasswords: 10
     }
 
     // We can't run any loops in render(), so a separate method should be created
     renderPasswords = numberOfPasswords => {
-        let passwordArray = [];
+        let passwordsArray = [];
+        const charactersList = this.generateCharactersList();
         for(let i = 1; i <= numberOfPasswords; i++) {
-            passwordArray.push(`${ this.generatePassword() }`);
+            passwordsArray.push(this.generatePassword(charactersList));
         }
-        return passwordArray;
+        return passwordsArray;
     }
 
     // Generating string of all characters that we'll need (depending on state.options.selected)
     generateCharactersList = () => {
         let charactersList = "";
         let { options } = this.state;
-        options.map(option => {
+        options.forEach(option => {
             if(option.selected) {
                 charactersList += option.characters;
             }
-            return false;
         });
         return charactersList;
     }
 
     // Generating single password
-    generatePassword = () => {
-        const charactersList = this.generateCharactersList();
+    generatePassword = charactersList => {
         // Creating Uint8Array-type array
         const randomNumbersArray = new Uint8Array( this.state.passwordLength );
-        // populating the new Uint8Array with random integers in the range of 0-255
+        // Populating the new Uint8Array with random integers in the range of 0-255
         // (because this is what it can hold)
         window.crypto.getRandomValues( randomNumbersArray );
         let password = "";
-        randomNumbersArray.map(value => {
+        randomNumbersArray.forEach(value => {
             // Calculating random integer in the range of 0-(charactersList.length-1)
-            // idea: https://stackoverflow.com/questions/1527803/
-            let j = Math.floor((value * charactersList.length) >> 8);
-            password += charactersList[j];
-            return false;
+            // inspired by: https://stackoverflow.com/questions/1527803/
+            // (only we do not need Math.floor() as bitwise operator >> is used)
+            const randomIndex = value * charactersList.length >> 8;
+            password += charactersList[randomIndex];
         });
         return password;
     };
