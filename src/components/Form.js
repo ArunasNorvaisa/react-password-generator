29255dd807e514e8c6a842951127375272644457
Arūnas Norvaiša <an@86.lt>
2019-02-06 10:15:57 +0200

    clipboard copy implementation started - still much to do

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,7 +26,7 @@ class Form extends Component {
         ],
         passwordLength: 15,
         numberOfPasswords: 10
-    }
+    };
 
     // We can't run any loops in render(), so a separate method should be created
     renderPasswords = numberOfPasswords => {
@@ -36,7 +36,7 @@ class Form extends Component {
             passwordsArray.push(this.generateSinglePassword(charactersList));
         }
         return passwordsArray;
-    }
+    };
 
     // Generating string of all characters that we'll need (depending on state.options.selected)
     generateCharactersList = () => {
@@ -48,7 +48,7 @@ class Form extends Component {
             }
         });
         return charactersList;
-    }
+    };
 
     // Generating single password
     generateSinglePassword = charactersList => {
@@ -74,7 +74,7 @@ class Form extends Component {
         if (event.target.value < 2) {
             this.setState({ passwordLength: 2 });
         }
-    }
+    };
 
     handleNumberOfPasswordsChange = event => {
         this.setState({ numberOfPasswords: event.target.value });
@@ -82,7 +82,7 @@ class Form extends Component {
         if (event.target.value < 1) {
             this.setState({ numberOfPasswords: 1 });
         }
-    }
+    };
 
     handleCharListChange = index => {
 
@@ -92,13 +92,76 @@ class Form extends Component {
 
             return { options: options };
         });
-    }
-
+    };
 
     render() {
+
+        // copy-to-clipboard code start
+        // ============================
+        // inspired by https://alligator.io/js/copying-to-clipboard/
+
+        // We first get all the span elements that contain the password values:
+
+        const generatedPasswords = document.querySelectorAll('.password span');
+
+        console.log(generatedPasswords);
+
+        // querySelectorAll return a nodeList, which is an array-like object.
+        // We then iterate over each node and add a click event listener:
+
+        generatedPasswords.forEach(password => {
+            password.addEventListener('click', () => {
+
+        // The magic happens next with the following code:
+        // This gets a selection object, then creates a range where we select
+        // the contents of our span element. We then make sure that the global
+        // selection doesn’t contain any range yet, and add our new range to the selection.
+
+                const selection = window.getSelection();
+                const range = document.createRange();
+                range.selectNodeContents(password);
+                selection.removeAllRanges();
+                selection.addRange(range);
+
+        // Next we’re ready to try and copy the selection to the clipboard
+
+                try {
+                    document.execCommand('copy');
+                    selection.removeAllRanges();
+
+                    const original = password.textContent;
+                    password.textContent = 'Copied!';
+                    password.classList.add('success');
+
+                    setTimeout(() => {
+                        password.textContent = original;
+                        password.classList.remove('success');
+                    }, 1200);
+                }
+
+        // We then remove the selection range on our selection object to deselect
+        // the text, and setup a success message that disappears after 1.2 seconds.
+
+        // In case of failure, we keep the selection active, and display a message
+        // to tell the user to hit Ctrl + C:
+
+                catch(e) {
+                    const errorMsg = document.querySelector('.error-msg');
+                    errorMsg.classList.add('show');
+
+                    setTimeout(() => {
+                        errorMsg.classList.remove('show');
+                    }, 1200);
+                }
+            });
+        });
+
+        // copy-to-clipboard code end
+        // ==========================
+
         const pwdArray = this.renderPasswords( this.state.numberOfPasswords );
         return <div className="container">
-            <div  className="initial_data">
+            <div>
                 {
                     this.state.options.map( (option, index) => {
                         return <div key = { option.name }>
@@ -126,7 +189,7 @@ class Form extends Component {
                 {
                     pwdArray.map((value, index) => {
                         return <div key = { index } className="password">
-                            <input type="text" value={ value } readOnly />
+                            <span>{ value }</span>
                         </div>
                     })
                 }
