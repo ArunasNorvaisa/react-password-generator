bdedc384378617cc9368930b4cde69b7272147f2
Arūnas Norvaiša <an@86.lt>
2019-02-07 14:06:55 +0200

    Copy to clipboard implemented, 'copied' alert added, CSS flexbox

--- src/components/Form.js
+++ src/components/Form.js
@@ -103,72 +103,7 @@ class Form extends Component {
     }
 
     render() {
-/*
-        // copy-to-clipboard code start
-        // ============================
-        // inspired by https://alligator.io/js/copying-to-clipboard/
 
-        // We first get all the span elements that contain the password values:
-
-        const generatedPasswords = document.querySelectorAll('.password span');
-
-        console.log(generatedPasswords);
-
-        // querySelectorAll return a nodeList, which is an array-like object.
-        // We then iterate over each node and add a click event listener:
-
-        generatedPasswords.forEach(password => {
-            password.addEventListener('click', () => {
-
-        // The magic happens next with the following code:
-        // This gets a selection object, then creates a range where we select
-        // the contents of our span element. We then make sure that the global
-        // selection doesn't contain any range yet, and add our new range to the selection.
-
-                const selection = window.getSelection();
-                console.log('selection:', selection);
-                const range = document.createRange();
-                console.log('range:', range);
-                range.selectNodeContents(password);
-                selection.removeAllRanges();
-                selection.addRange(range);
-
-        // Next we’re ready to try and copy the selection to the clipboard:
-
-                try {
-                    document.execCommand('copy');
-                    selection.removeAllRanges();
-
-                    const original = password.textContent;
-                    password.textContent = 'Copied!';
-                    //password.classList.add('success');
-
-                    setTimeout(() => {
-                        password.textContent = original;
-                        //password.classList.remove('success');
-                    }, 1200);
-                }
-
-        // We then remove the selection range on our selection object to deselect
-        // the text, and setup a success message that disappears after 1.2 seconds.
-
-        // In case of failure, we keep the selection active, and display a message
-        // to tell the user to hit Ctrl + C:
-
-                catch(e) {
-                    const errorMsg = document.querySelector('.error-msg');
-                    errorMsg.classList.add('show');
-
-                    setTimeout(() => {
-                        errorMsg.classList.remove('show');
-                    }, 1200);
-                }
-            });
-        });
-
-        // copy-to-clipboard code end
-        // ==========================
-*/
         const pwdArray = this.renderPasswords( this.state.numberOfPasswords );
         return <div className="container">
             <div className="initial_options">
@@ -199,8 +134,6 @@ class Form extends Component {
             <div className="generated_passwords">
                 {
                     pwdArray.map((value, index) => {
-                        console.log('index:', index)
-                        console.log('value:', value)
                         return <div key = { index } className="password">
                             <input
                                 type="text"
