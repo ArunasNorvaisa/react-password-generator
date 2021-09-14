f9841729be1fb90092232f34ff06bd32598c2bad
Arunas Norvaisa <arunas.norvaisa@bdo.global>
2021-09-14 17:10:39 +0300

    introduced Functor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import * as styles from '../css/styles.scss';
 
+const Functor = v => ({
+  map: f => Functor(f(v)),
+  out: f => f(v)
+});
+
 const initialState = {
   options: [
     {
@@ -33,38 +38,42 @@ function App() {
   const [state, setState] = useState(initialState);
   const { numberOfPasswords, options, passwordLength } = state;
 
-  function renderPasswords(numberOfPasswords) {
-    const passwordsArray = [];
-    const charactersList = generateCharactersList();
-    for (let i = 1; i <= numberOfPasswords; i++) {
-      passwordsArray.push(generateSinglePassword(charactersList));
-    }
-
-    return passwordsArray;
+  function renderPasswords(howMany) {
+    return Functor([])
+      .out(passwords => {
+        for (let i = 1; i <= howMany; i++) {
+          Functor()
+            .map(generateCharactersList)
+            .map(charList => generateSinglePassword(charList))
+            .out(password => passwords.push(password))
+        }
+        return passwords;
+      })
   }
 
   function generateCharactersList() {
-    let charactersList = '';
-    options.forEach(option => {
-      if (option.selected) {
-        charactersList += option.characters;
-      }
-    });
-
-    return charactersList;
+    return Functor('')
+      .out(charList => {
+        options.forEach(option => {
+          if (option.selected) {
+            charList += option.characters;
+          }
+        });
+        return charList;
+      })
   }
 
-  function generateSinglePassword(charactersList) {
-    const randomNumbersArray = new Uint16Array(state.passwordLength);
+  function generateSinglePassword(characterList) {
+    const randomNumbersArray = new Uint16Array(passwordLength);
     crypto.getRandomValues(randomNumbersArray);
     let password = '';
-    randomNumbersArray.forEach(value => {
+    for (const number of randomNumbersArray) {
       // Calculating random integer in the range of 0-(charactersList.length-1)
       // inspired by: https://stackoverflow.com/questions/1527803/
       // (only we do not need Math.floor() as bitwise operator >> is used)
-      const randomIndex = value * charactersList.length >> 16;
-      password += charactersList[randomIndex];
-    });
+      const randomIndex = number * characterList.length >> 16;
+      password += characterList[randomIndex];
+    }
 
     return password;
   }
@@ -99,15 +108,15 @@ function App() {
 
   // Below function was shamelessly copied from https://stackoverflow.com/questions/45071353
   function copyToClipboard(str) {
-    const el = document.createElement('textarea');          // Create a <textarea> element
+    const el = document.createElement('textarea'); // Create a <textarea> element
     el.value = str;                                         // Set its value to the string that you want copied
-    el.setAttribute('readonly', '');                        // Make it readonly to be tamper-proof
+    el.setAttribute('readonly', '');      // Make it readonly to be tamper-proof
     document.body.appendChild(el);                          // Append the <textarea> element to the HTML document
     const selected = document.getSelection().rangeCount > 0 // Check if there is any content selected previously
-      ? document.getSelection().getRangeAt(0)               // Store selection if found
+      ? document.getSelection().getRangeAt(0)         // Store selection if found
       : false;                                              // Mark as false to know no selection existed before
     el.select();                                            // Select the <textarea> content
-    document.execCommand('copy');                           // Copy - only works as a result of a user action (e.g. click events)
+    document.execCommand('copy');                // Copy - only works as a result of a user action (e.g. click events)
     document.body.removeChild(el);                          // Remove the <textarea> element
     if (selected) {                                         // If a selection existed before copying
       document.getSelection().removeAllRanges();            // Unselect everything on the HTML document
@@ -120,43 +129,51 @@ function App() {
   return <div className={styles.container}>
     <table>
       <tbody>
-        {
-          options.map((option, index) => {
-            return <tr key={option.name}>
-              <td><div className={styles.description}>{option.name}</div></td>
-              <td><div className={styles.description}>{option.characters}</div></td>
-              <td>
-                <input
-                  type="checkbox"
-                  checked={option.selected}
-                  onChange={() => handleCharListChange(index)}
-                />
-              </td>
-            </tr>
-          })
-        }
-        <tr>
-          <td><div className={styles.description}>Password Length</div></td>
-          <td>
-            <input
-              type="number"
-              min="2"
-              value={passwordLength}
-              onChange={e => setState({ ...state, passwordLength: e.target.value })}
-            />
-          </td>
-        </tr>
-        <tr>
-          <td><div className={styles.description}>Number of Passwords</div></td>
-          <td>
-            <input
-              type="number"
-              min="1"
-              value={numberOfPasswords}
-              onChange={e => setState({ ...state, numberOfPasswords: e.target.value })}
-            />
-          </td>
-        </tr>
+      {
+        options.map((option, index) => {
+          return <tr key={option.name}>
+            <td>
+              <div className={styles.description}>{option.name}</div>
+            </td>
+            <td>
+              <div className={styles.description}>{option.characters}</div>
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={option.selected}
+                onChange={() => handleCharListChange(index)}
+              />
+            </td>
+          </tr>;
+        })
+      }
+      <tr>
+        <td>
+          <div className={styles.description}>Password Length</div>
+        </td>
+        <td>
+          <input
+            type="number"
+            min="2"
+            value={passwordLength}
+            onChange={e => setState({ ...state, passwordLength: e.target.value })}
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <div className={styles.description}>Number of Passwords</div>
+        </td>
+        <td>
+          <input
+            type="number"
+            min="1"
+            value={numberOfPasswords}
+            onChange={e => setState({ ...state, numberOfPasswords: e.target.value })}
+          />
+        </td>
+      </tr>
       </tbody>
     </table>
     <h1>Generated passwords</h1>
@@ -168,11 +185,11 @@ function App() {
             <span id={`password${index}`} onClick={handleClipboardCopy}>
               {value}
             </span>
-          </p>
+          </p>;
         })
       }
     </div>
-  </div>
+  </div>;
 }
 
 export default App;
